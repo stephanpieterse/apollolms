f7674bacf3cdba77279ef42a9d67bc3ca43613c7
Stephan Pieterse <stephanpieterse@rubyhemisphere.dedicated.co.za>
2014-12-22 09:37:20 +0200

    Not sure... but is latest

--- courses.php
+++ courses.php
@@ -4,5 +4,6 @@
 	$control->secNav = 'courses.php';
 	$control->formPre = 'courses_form_';
 	$control->funcPre = 'courses_func_';
+	$control->protectedPages = true;
 	$control->executeControl($_GET,$_POST);
 ?>
