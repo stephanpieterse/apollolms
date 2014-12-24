e703e21a1325676448c6c73818010d9d380f8732
Stephan Pieterse <stephanpieterse@rubyhemisphere.dedicated.co.za>
2014-12-24 07:35:27 +0000

    Fixed logging causing mysql error

--- func_misc.php
+++ func_misc.php
@@ -532,6 +532,7 @@ function logAction($getDataSet,$customData = ""){
 		$ref = $rootNode->appendChild($newNode);			
 		
 	$xmlData = $xmlDoc->saveHTML();
+	$xmlData = mysqli_real_escape_string($xmlData);
 	$q = "UPDATE member_view_history SET history='$xmlData' WHERE uid='" . $_SESSION['userID'] . "'";
 	$r = sql_execute($q);
 	
